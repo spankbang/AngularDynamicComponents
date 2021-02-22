1ccf57f90652f6d17e6db26f58a60b147becdf62
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-22 10:33:48 +0530

    added the new alert file and as well the first dynamic content !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isItTrue:boolean= false
   title = 'AngularDynamicComponents';
+  error = "fuck this shit !"
+
+  clicked() {
+    this.isItTrue = !this.isItTrue
+    console.log(this.isItTrue)
+  }
+
 }
